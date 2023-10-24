858fff32e8cee171601c1bd4c95c359a98cab948
GFX23 <Spacil-Tomas@seznam.cz>
2023-10-24 08:47:38 +0200

    final setup graphql/express

--- server/src/graphQL/resolvers.ts
+++ server/src/graphQL/resolvers.ts
@@ -1,18 +1,33 @@
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
+import people from "../mongobase/database"; //get all of the available data from our database.
 
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
+interface Person {
+  id: number;
+  name: string;
+}
+
+interface Args {
+  id: number;
+  name: string;
+}
+
+const Resolvers = {
+  Query: {
+    getAllPeople: (): Person[] => people,
+    getPerson: (_: any, args: Args): Person | null => {
+      console.log(args);
+      const foundPerson = people.find((person) => person.id === args.id);
+      return foundPerson || null;
+    },
   },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
+  Mutation: {
+    addPerson: (_: any, args: Args): Person => {
+      const newPerson: Person = {
+        id: people.length + 1,
+        name: args.name,
+      };
+      people.push(newPerson);
+      return newPerson;
+    },
   },
-];
+}
+export default Resolvers; //export this Resolver so we can use it in our project
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,25 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from "apollo-server-express";
+import Schema from "./graphql/schema";
+import Resolvers from "./graphql/resolvers";
+import express from "express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
 
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
-
-console.log(`🚀  Server ready at: ${url}`);
-
-
+async function startApolloServer(schema: any, resolvers: any) {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+    //tell Express to attach GraphQL functionality to the server
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  }) as any;
+  await server.start(); //start the GraphQL server.
+  server.applyMiddleware({ app });
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve) //run the server on port 4000
+  );
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+}
+//in the end, run the server and pass in our Schema and Resolver.
+startApolloServer(Schema, Resolvers);
