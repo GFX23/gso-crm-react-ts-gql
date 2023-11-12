39032fbaffaf60d7ed860a0e0270802044d1cda9
GFX23 <Spacil-Tomas@seznam.cz>
2023-11-12 19:36:47 +0100

    added new Button template

---
+++ client/src/components/ButtonUtil.tsx
@@ -0,0 +1,40 @@
+// Button component for delete and confirmation
+
+type ButtonProps = {
+  type: "green" | "red" | "blue";
+  state?: boolean;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ type ,state, onClick }) => {
+  if (type === "green") {
+     return (
+      <button
+        className="w-8 h-8 pt-2 flex items-center justify-center rounded-full bg-green-400 border-1 border-black hover:bg-green-500"
+        onClick={onClick}
+      >
+        &#10003;
+      </button>
+    );}
+  else if (type === "red") {
+    return (
+      <button
+        className="w-8 h-8 flex items-center justify-center rounded-full bg-red-400 border-1 border-black hover:bg-red-500"
+        onClick={onClick}
+      >
+        &#10005;
+      </button>
+    );
+  } else if (type === "blue") {
+    return (
+      <button 
+        className="w-8 h-8 flex pb-1.5 text-lg items-center justify-center rounded-full bg-blue-400 border-1 border-black hover:bg-blue-500"
+        onClick={onClick}
+      >
+        {state ? "\u2191" : "\u2193"}
+      </button>
+    );
+  }
+}
+
+export default Button;
