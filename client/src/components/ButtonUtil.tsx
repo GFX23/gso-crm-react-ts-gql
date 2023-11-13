d7697ea007f9828c809eca4fea9169a7479981a7
GFX23 <Spacil-Tomas@seznam.cz>
2023-11-13 09:21:44 +0100

    changed styles

--- client/src/components/ButtonUtil.tsx
+++ client/src/components/ButtonUtil.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps> = ({ type ,state, onClick }) => {
   if (type === "green") {
      return (
       <button
-        className="w-8 h-8 pt-2 flex items-center justify-center rounded-full bg-green-400 border-1 border-black hover:bg-green-500"
+        className="w-8 h-8 pt-2 flex items-center justify-center rounded-full bg-green-400 border-1 hover:bg-green-500"
         onClick={onClick}
       >
         &#10003;
@@ -19,7 +19,7 @@ const Button: React.FC<ButtonProps> = ({ type ,state, onClick }) => {
   else if (type === "red") {
     return (
       <button
-        className="w-8 h-8 flex items-center justify-center rounded-full bg-red-400 border-1 border-black hover:bg-red-500"
+        className="w-8 h-8 flex items-center justify-center rounded-full bg-red-400 border-1 hover:bg-red-500"
         onClick={onClick}
       >
         &#10005;
@@ -28,7 +28,7 @@ const Button: React.FC<ButtonProps> = ({ type ,state, onClick }) => {
   } else if (type === "blue") {
     return (
       <button 
-        className="w-8 h-8 flex pb-1.5 text-lg items-center justify-center rounded-full bg-blue-400 border-1 border-black hover:bg-blue-500"
+        className="w-8 h-8 flex pb-1.5 text-lg items-center justify-center rounded-full bg-blue-400 border-1 hover:bg-blue-500"
         onClick={onClick}
       >
         {state ? "\u2191" : "\u2193"}
